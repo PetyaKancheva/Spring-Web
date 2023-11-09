e296342d99aa9e0873819ad522583d61cfdd8c99
PetyaKancheva <105904595+PetyaKancheva@users.noreply.github.com>
2023-11-09 08:10:54 +0100

    Build up app.

---
+++ bike-shop/src/main/java/bg/softuni/bikeshop/model/entity/UserEntity.java
@@ -0,0 +1,11 @@
+package bg.softuni.bikeshop.model.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="users")
+
+public class UserEntity {
+
+}
