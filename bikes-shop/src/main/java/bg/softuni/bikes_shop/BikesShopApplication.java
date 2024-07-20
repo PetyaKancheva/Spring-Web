402b2f2645b6bb36b1bd14201ac86932cce078f3
PetyaKancheva <105904595+PetyaKancheva@users.noreply.github.com>
2024-07-20 12:22:11 +0200

    Start project

---
+++ bikes-shop/src/main/java/bg/softuni/bikes_shop/BikesShopApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.bikes_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikesShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikesShopApplication.class, args);
+	}
+
+}
