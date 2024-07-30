18f8c2888f421b7107d4131989f20b58fa37f5d9
PetyaKancheva <105904595+PetyaKancheva@users.noreply.github.com>
2024-07-30 20:22:49 +0200

    Adding thymeleaf

--- bikes-shop/src/main/java/bg/softuni/bikes_shop/controller/homeController.java
+++ bikes-shop/src/main/java/bg/softuni/bikes_shop/controller/homeController.java
@@ -3,14 +3,19 @@ package bg.softuni.bikes_shop.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 // TODO check REst controller
 
-@RestController
+@Controller
 public class homeController {
-
     @GetMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
+    private String index(){
+        return "index";
+    }
 
-}
+    @GetMapping("/about")
+    private String about(){
+        return "about";
+    }
+
 }
