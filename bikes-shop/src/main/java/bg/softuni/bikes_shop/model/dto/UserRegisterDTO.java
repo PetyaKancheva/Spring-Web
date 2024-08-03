a21a6c2d6865e2ccadfc6cf0bdc5acacb916644e
PetyaKancheva <105904595+PetyaKancheva@users.noreply.github.com>
2024-08-03 15:07:48 +0200

    Building up  register page

--- bikes-shop/src/main/java/bg/softuni/bikes_shop/model/dto/UserRegisterDTO.java
+++ bikes-shop/src/main/java/bg/softuni/bikes_shop/model/dto/UserRegisterDTO.java
@@ -19,7 +19,8 @@ public record UserRegisterDTO(
         @Size(min = 2, message= "Must be at least 2 characters.")
         String password,
         @NotEmpty(message ="Cannot be empty.")
-        String confirmPassword)  {
+        String confirmPassword)
+            {
     public static UserRegisterDTO empty() {
         return new UserRegisterDTO(null, null, null, null, null);
     }
