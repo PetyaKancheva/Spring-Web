3eb2e6b09364fc5195f9957598e5a2a7ce8bdee4
PetyaKancheva <105904595+PetyaKancheva@users.noreply.github.com>
2024-08-04 13:27:15 +0200

    Building up registration

--- bikes-shop/src/main/java/bg/softuni/bikes_shop/controller/homeController.java
+++ bikes-shop/src/main/java/bg/softuni/bikes_shop/controller/homeController.java
@@ -11,14 +11,20 @@ import org.springframework.web.bind.annotation.RestController;
 @Controller
 public class homeController {
     @GetMapping("/")
-
     private String index(){
         return "index";
     }
-
     @GetMapping("/about")
     private String about(){
         return "about";
+    }
+    @GetMapping("/services")
+    private String services(){
+        return "services";
+    }
+    @GetMapping("/contacts")
+    private String contacts(){
+        return "contacts";
     }
 
 }
