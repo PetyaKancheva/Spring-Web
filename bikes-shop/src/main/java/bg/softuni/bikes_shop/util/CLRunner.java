69210e0ee3c874740139750403dc31e0946b35a5
PetyaKancheva <105904595+PetyaKancheva@users.noreply.github.com>
2025-03-27 13:12:45 +0100

    Fixing small issues

--- bikes-shop/src/main/java/bg/softuni/bikes_shop/util/CLRunner.java
+++ bikes-shop/src/main/java/bg/softuni/bikes_shop/util/CLRunner.java
@@ -1,6 +1,9 @@
 package bg.softuni.bikes_shop.util;
 
 
+import bg.softuni.bikes_shop.model.UserRoleEnum;
+import bg.softuni.bikes_shop.model.entity.UserRoleEntity;
+import bg.softuni.bikes_shop.repository.UserRoleRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -9,13 +12,20 @@ import org.springframework.stereotype.Component;
 @Component
 public class CLRunner implements CommandLineRunner {
 
-    public CLRunner() {
+    private final UserRoleRepository userRoleRepository;
 
+    public CLRunner(UserRoleRepository userRoleRepository) {
+        this.userRoleRepository = userRoleRepository;
     }
+
 
     @Override
     public void run(String... args) throws Exception {
-
+        if(userRoleRepository.count()==0){
+            userRoleRepository.save(new UserRoleEntity().setName(UserRoleEnum.USER));
+            userRoleRepository.save(new UserRoleEntity().setName(UserRoleEnum.ADMIN));
+            userRoleRepository.save(new UserRoleEntity().setName(UserRoleEnum.EMPLOYEE));
+        }
 
     }
 
